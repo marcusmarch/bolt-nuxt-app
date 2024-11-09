ea1a232a40b4a8788e3c2950682fefbf86b63e94
Marcus <marcusmarch@users.noreply.github.com>
2024-11-09 13:50:21 +0000

    more events, add utc times so date displays what I wanted

--- data/timelineEvents.ts
+++ data/timelineEvents.ts
@@ -1,20 +1,27 @@
 export const timelineEvents = [
   {
-    date: new Date("2024-11-05"),
+    date: new Date("2024-11-05T00:00:00Z"),
     title: "Wins the Presidency",
     description:
       "Wins with the Popular Vote. Voters apparently reject Biden/Harris's last 4 years. Harris calls Trump to concede.",
     image: "https://picsum.photos/seed/1/200/100",
   },
   {
-    date: new Date("2024-11-06"),
+    date: new Date("2024-11-06 11:00:00"),
     title: "Hamas Demands Peace",
     description: "",
     image: "https://picsum.photos/seed/1/200/100",
     link: "https://www.newsweek.com/hamas-calls-immediate-end-war-after-trump-election-win-1981571",
   },
   {
-    date: new Date("2024-11-08"),
+    date: new Date("2024-11-08 11:00:00"),
+    title: "World Leaders React",
+    description: "",
+    image: "https://picsum.photos/seed/1/200/100",
+    link: "https://x.com/itzjoshuajake/status/1854954310486159562",
+  },
+  {
+    date: new Date("2024-11-08 11:00:00"),
     title: "Trump Demands Free Speech for Americans",
     description:
       "Typical of a would-be Dictator, demanding free speech for all Americans. Promises to punish any who prevent you from speaking your mind online",
