58cd4ac387d875224d646e75c232d293ebc3ff04
Marcus <marcusmarch@users.noreply.github.com>
2024-11-09 13:35:02 +0000

    typo and cleanup

--- data/timelineEvents.ts
+++ data/timelineEvents.ts
@@ -11,13 +11,13 @@ export const timelineEvents = [
     title: "Hamas Demands Peace",
     description: "",
     image: "https://picsum.photos/seed/1/200/100",
-    link: "google.com",
+    link: "https://www.newsweek.com/hamas-calls-immediate-end-war-after-trump-election-win-1981571",
   },
   {
     date: new Date("2024-11-08"),
-    title: "Demands Free Speach",
+    title: "Trump Demands Free Speech for Americans",
     description:
-      "Typical of a would-be Dictator, demanding free speach for all Americans.",
+      "Typical of a would-be Dictator, demanding free speech for all Americans. Promises to punish any who prevent you from speaking your mind online",
     image: "https://picsum.photos/seed/2/200/100",
     link: "https://x.com/LibertyLockPod/status/1854949449518514593",
     linkText: "View the announcment here",
